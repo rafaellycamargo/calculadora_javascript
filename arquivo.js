f7f4c298fdc2cf153aaaedcb37d4e4ab9155dd00
Rafaelly Camargo <rafaelly.c@aluno.ifsp.edu.br>
2020-09-29 17:03:10 -0300

    Adicionei os arquivos html e js

---
+++ arquivo.js
@@ -0,0 +1,8 @@
+var numero1 = prompt("Digite o primeiro valor:");
+var numero2 = prompt("Digite o segundo valor:");
+var int1 = parseInt(numero1);
+var int2 = parseInt(numero2);
+var resultado = int1 + int2;
+console.log(resultado);
+
+alert("Resultado = " + resultado);
